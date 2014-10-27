246c1ddd37e175913bfe8740913434098a967404
Jamie Maddocks <jamie@chicken-studios.com>
2014-10-27 00:24:56 +0000

    Now uses spaces instead of tabs as per PEP guidelines :(

--- set-1/challenge_2.py
+++ set-1/challenge_2.py
@@ -1,13 +1,13 @@
 def fixed_xor(value_1, value_2):
-	value_1 = bytearray.fromhex(value_1)
-	value_2 = bytearray.fromhex(value_2)
-	xored = [a ^ b for a, b in zip(value_1, value_2)]
-	return xored
+    value_1 = bytearray.fromhex(value_1)
+    value_2 = bytearray.fromhex(value_2)
+    xored = [a ^ b for a, b in zip(value_1, value_2)]
+    return xored
 
 if __name__ == "__main__":
-	xor_data = fixed_xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
-	xor_string = ""
-	for x in xor_data:
-		xor_string += ("%x" % x)
+    xor_data = fixed_xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
+    xor_string = ""
+    for x in xor_data:
+        xor_string += ("%x" % x)
 
-	print xor_string
+    print xor_string
