be3a3adf82aabafa735834aae6bc208c4490fb13
Jamie Maddocks <jamie@chicken-studios.com>
2014-10-27 00:19:14 +0000

    Challenge 3 completed

---
+++ set-1/challenge_1.py
@@ -0,0 +1,44 @@
+import sys
+
+hex_bytes = bytearray.fromhex(sys.argv[1])
+
+# Most definitely a faster way to do this but I wanted to write it myself
+# if anyone wants to run me through it that'd be great thanks
+base_64_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+def hex_to_base64(byte_string):
+	offset = 0
+	length = len(byte_string)
+	conv_array = bytearray()
+
+	pad_bytes = 3 - (length % 3)
+	if (pad_bytes == 3):
+		pad_bytes = 0
+
+	for i in xrange(pad_bytes):
+		print "padded"
+		byte_string.append(0x00)
+
+	while (length > 0):
+		index = ((byte_string[0 + offset] & 0xFC) >> 2)
+		conv_array.append(base_64_table[index])
+
+		index = ((((byte_string[0 + offset] & 0x03) << 4) | 
+			((byte_string[1 + offset] & 0xF0) >> 4)))
+
+		conv_array.append(base_64_table[index])
+
+		index = (((byte_string[1 + offset] & 0x0F) << 2) | 
+			((byte_string[2 + offset] & 0xC0) >> 6))
+		conv_array.append(base_64_table[index])
+
+		index = (byte_string[2 + offset] & 0x3F)
+		conv_array.append(base_64_table[index])
+		
+		offset += 3
+		length -= 3
+	
+	for i in xrange(pad_bytes):
+		conv_array.append('=')
+
+	return conv_array
---
+++ set-1/challenge_3.py
@@ -0,0 +1,35 @@
+from challenge_2 import fixed_xor 
+
+def brute_xor(ciphertext):
+	cipher_len = len(ciphertext)
+	best_guess = (0, "")
+
+	for key in range(0, 255):
+		long_key =  str(key) * cipher_len
+
+		result = fixed_xor(ciphertext, long_key)
+		hex_string = ''.join(["%02x" % a for a in result])
+		plaintext = hex_string.decode("hex")
+		score = score_text(plaintext)
+
+		if (score > best_guess[0]):
+			best_guess = (score, plaintext)
+
+	return best_guess
+
+# A /very very/ simple scoring that could be made a lot more complex but at this stage
+# what is the point? There's got to be a nicer way than this huge if statement too
+def score_text(plaintext):
+	score = 0
+	for c in plaintext.lower():
+		if (c == 'e' or c == 't' or c == 'a' or c == 'o' or c == 'i' or c == 'n'
+				or c == 's' or c == 'h' or c == 'r' or c == 'd' or c== 'l' or c == 'u'
+				or c == ' '):
+			score += 1
+
+	return score
+
+if __name__ == "__main__":
+	code = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	print brute_xor(code)[1]
+	
